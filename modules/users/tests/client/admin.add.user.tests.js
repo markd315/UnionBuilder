6dc3d705e53449e73c91af9598703582a7ffcf13
kevdaci <kevdaci@gmail.com>
2017-11-15 12:04:45 -0500

    Add beforeEach injection

--- modules/users/tests/client/admin.add.user.tests.js
+++ modules/users/tests/client/admin.add.user.tests.js
@@ -1,9 +1,32 @@
-
-
 (function () {
 	'use strict';
 
 	describe('AddUserController', function() {
+		var AuthenticationController,
+      		scope,
+      		$httpBackend,
+      		$stateParams,
+      		$state,
+      		$location,
+      		Notification;
+
+		beforeEach(function () {
+	    	jasmine.addMatchers({
+	    		toEqualData: function (util, customEqualityTesters) {
+	        		return {
+	            		compare: function (actual, expected) {
+	              			return {
+	                			pass: angular.equals(actual, expected)
+	              			};
+	           			 }
+	          		};
+	        	}
+	      	});
+	 	});
+
+	 	beforeEach(module(ApplicationConfiguration.applicationModuleName));
+
+
 
 	});
 });
