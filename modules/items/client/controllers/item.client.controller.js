ba868c04dfbc3752d7eb7b08b9a6ed96ce448c66
DrFlu <drfluju@ufl.edu>
2017-11-21 18:23:59 -0500

    changed to resolve a merge conflict

--- modules/items/client/controllers/item.client.controller.js
+++ modules/items/client/controllers/item.client.controller.js
@@ -3,7 +3,7 @@
 
   angular
     .module('items')
-    .controller('ItemsAdminController', ItemsAdminController);
+    .controller('ItemsController', ItemsController);
 
     ItemsAdminController.$inject = ['$scope', '$state', '$window', 'itemResolve', 'CategoriesService', 'ModulesService', 'Authentication', 'Notification'];
 
