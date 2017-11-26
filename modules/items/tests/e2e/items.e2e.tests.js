f77fe00b734c651d6f42ed9af3d2842c14f84467
Cianan_Roden-Corrent <crodencorrent@ufl.edu>
2017-11-26 15:27:07 -0500

    Attempting to fix some errors with the database not recogonizing a user has already been registered.

--- modules/items/tests/e2e/items.e2e.tests.js
+++ modules/items/tests/e2e/items.e2e.tests.js
@@ -1,42 +1,9 @@
 'use strict';
 
 describe('Items E2E Tests:', function () {
-	var user1 = {
-    firstName: 'test',
-    lastName: 'user',
-    email: 'test.user@meanjs.com',
-    username: 'testUser',
-    password: 'P@$$w0rd!!',
-    roles: 'TA'
-  };
-
-  var user2 = {
-    firstName: 'test',
-    lastName: 'user2',
-    email: 'test.user2@meanjs.com',
-    username: 'testUser2',
-    password: 'P@$$w0rd!!',
-    roles: 'TA'
-  };
   var admin = {
     username: 'seedadmintest',
     password: 'P@$$w0rd!!'
-  };
-  var user3 = {
-    firstName: 'test',
-    lastName: 'user',
-    email: 'test.user3@meanjs.com',
-    username: 'testUser3',
-    password: 'P@$$w0rd!!',
-    roles: 'TA'
-  };
-  var user4 = {
-    firstName: 'test',
-    lastName: 'user4',
-    email: 'test.user4@meanjs.com',
-    username: 'testUser4',
-    password: 'P@$$w0rd!!',
-    roles: 'TA'
   };
   var testItem = {
   	title: 'title',
@@ -70,8 +37,8 @@ describe('Items E2E Tests:', function () {
       element(by.css('button[type="submit"]')).click();
   };
   describe('Test items page', function () {
-  	signin();
-  	it('Should navigate to item page with an admin login', function () {
+    signin();
+    it('Should navigate to item page with an admin login', function () {
      browser.get('http://localhost:3001/items');
      expect(browser.getCurrentUrl()).toEqual('http://localhost:3001/items');
     });
@@ -98,8 +65,8 @@ describe('Items E2E Tests:', function () {
     });
   });
   describe('Test Categories, Modules pages', function () {
-  	signin();
-  	it('Should navigate to category page with an admin login', function () {
+    signin();
+    it('Should navigate to category page with an admin login', function () {
      browser.get('http://localhost:3001/admin/items/categories');
      expect(browser.getCurrentUrl()).toEqual('http://localhost:3001/admin/items/categories');
     });
@@ -112,8 +79,8 @@ describe('Items E2E Tests:', function () {
      browser.get('http://localhost:3001/admin/items/categories/create');
      element(by.css('button[type=submit]')).click();
      var alertDialog = browser.switchTo().alert();
-  	expect(alertDialog.accept).toBeDefined();
- 	 alertDialog.accept();
+     expect(alertDialog.accept).toBeDefined();
+     alertDialog.accept();
      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('Category name is required.');
     });
     it('Should create an category with a title', function () {
@@ -121,8 +88,8 @@ describe('Items E2E Tests:', function () {
      element(by.model('vm.cat.title')).sendKeys(category.title);
      element(by.css('button[type=submit]')).click();
      var alertDialog = browser.switchTo().alert();
-  	 expect(alertDialog.accept).toBeDefined();
- 	 alertDialog.accept();
+     expect(alertDialog.accept).toBeDefined();
+     alertDialog.accept();
       expect(browser.getCurrentUrl()).toEqual('http://localhost:3001/admin/items/categories');
     });
     it('Should navigate to module page with an admin login', function () {
@@ -138,8 +105,8 @@ describe('Items E2E Tests:', function () {
      browser.get('http://localhost:3001/admin/items/modules/create');
      element(by.css('button[type=submit]')).click();
      var alertDialog = browser.switchTo().alert();
-  	 expect(alertDialog.accept).toBeDefined();
- 	 alertDialog.accept();
+     expect(alertDialog.accept).toBeDefined();
+     alertDialog.accept();
      expect(element.all(by.css('.error-text')).get(0).getText()).toBe('Module name is required.');
     });
     it('Should create an module with a title', function () {
@@ -147,9 +114,9 @@ describe('Items E2E Tests:', function () {
      element(by.model('vm.mod.title')).sendKeys(module.title);
      element(by.css('button[type=submit]')).click();
      var alertDialog = browser.switchTo().alert();
-  	 expect(alertDialog.accept).toBeDefined();
- 	 alertDialog.accept();
-      expect(browser.getCurrentUrl()).toEqual('http://localhost:3001/admin/items/modules');
+     expect(alertDialog.accept).toBeDefined();
+     alertDialog.accept();
+     expect(browser.getCurrentUrl()).toEqual('http://localhost:3001/admin/items/modules');
     });
 });
 });
