bfe6617b525c859d0732f310295a6e888a7042b4
markd315 <markd315@gmail.com>
2017-12-19 01:49:12 -0500

    Changed DB keys from forked project

--- config/env/development.js
+++ config/env/development.js
@@ -4,7 +4,7 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://InventoryApp:InventoryApp@ds157614.mlab.com:57614/cen3101-mean-dev',
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://gitTestURI:openpass@ds161713.mlab.com:61713/mark-mean',
     options: {},
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
